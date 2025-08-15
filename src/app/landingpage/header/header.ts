4f434134003d78363fd3789ca236dda94aedaf70
Isabel Egli <isabel.egli@bluewin.ch>
2025-08-15 05:18:44 +0200

    make responsive and code mobile menu

--- src/app/landingpage/header/header.ts
+++ src/app/landingpage/header/header.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   standalone: true,
 })
 export class Header {
- 
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  
 }
