4fb37d29fc315e59a76462ba8c4b82cf69c00186
Isabel Egli <isabel.egli@bluewin.ch>
2025-08-19 22:46:05 +0200

    fix bug with contactform

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Translations } from '../translations';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
